14b0bcb9b5501e78d385a5c9e80f6ce0613ec5c1
Hannan Yussof <21FTT1293@student.pb.edu.bn>
2023-04-10 19:13:12 +0800

    Modified tailwind.config.js

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,26 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <body class="bg-red-100">
+    <body class="bg-red-100 ">
     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-
         <div class="md:flex">
           <div class="md:shrink-0">
-            <img class="h-48 w-full object-cover md:h-full md:w-48" src="ITME.jpg">
+            <img class="rounded-lg h-48 w-full object-cover md:h-full md:w-48" src="ITME.jpg">
           </div>
-          
           <div class="p-8">
             <div class="uppercase tracking-wide text-sm font-semibold">Hannan Yussof</div>
-            <div class="block mt-1 text-lg leading-tight font-medium text-black">21FTT1293</div>
-            
-            <p class="mt-2 text-slate-500">Assignment 3 of module Web Application and Development Tools - WD4308</p>
+            <div class="block mt-1 text-lg leading-tight font-medium text-black">21FTT1293 - ASG3 WADT, WD4308</div>
+            <p class="mt-2 text-slate-500">I like ducks and alligators. Also like watching videos of capybaras and sending them to my sisters.</p>
           </div>
-
         </div>
       </div>
+
+
   </body>
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -35,5 +33,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
